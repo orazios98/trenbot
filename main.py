729cc5e3a1cf7704c133207ac2ea4605b99ad09c
Orazio <orazio.sciuto98@gmail.com>
2017-10-20 11:28:00 +0200

    aggiunta stampa e stazione di arrivo

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import logging
 import requests
 import json
 
-
 stazione_partenza=""
 stazione_arrivo=""
+flag=True
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -24,7 +24,9 @@ def start(bot, update):
     update.message.reply_text('Ciao!')
 
 
-def set(bot, update, args):
+def setp(bot, update, args):
+    global stazione_partenza
+    global flag
     lunarg=len(args)
     chat_id = update.message.chat_id
     if (lunarg>0):
@@ -46,7 +48,48 @@ def set(bot, update, args):
             if (lungh==1):
                 cit= d[0]['name']
                 update.message.reply_text("Citta' salvata: "  + cit)
+                stazione_partenza=cit
             else:
+                flag=True
+                for i in range(0, lungh):
+                    cit= d[i]['name']
+                    keyboard.append([InlineKeyboardButton(cit, callback_data=cit)])
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                update.message.reply_text('Please choose:', reply_markup=reply_markup)
+        else:
+            update.message.reply_text('Nessuna stazione corrispondente, riprova!')
+    else:
+        update.message.reply_text('Inserisci un valore valido!')
+
+
+def seta(bot, update, args):
+    global stazione_arrivo
+    global flag
+    lunarg=len(args)
+    chat_id = update.message.chat_id
+    if (lunarg>0):
+        keyboard=[[]]
+        ind=""
+        a1 = args[0]
+        ind = 'https://www.lefrecce.it/msite/api/geolocations/locations?name=' + a1
+        if(lunarg>1):   
+            for i in range(1, lunarg):
+                ind= ind+ "%20" + args[i]
+        print(ind)
+        r = requests.get(ind)
+        risp=r.text
+        print risp
+        d = json.loads(risp)
+        lungh=len(d)
+        print(lungh)
+        if(lungh>0):
+            if (lungh==1):
+                cit= d[0]['name']
+                update.message.reply_text("Citta' salvata: "  + cit)
+                stazione_arrivo=cit
+            else:
+                flag=False
+                print("sono qui")
                 for i in range(0, lungh):
                     cit= d[i]['name']
                     keyboard.append([InlineKeyboardButton(cit, callback_data=cit)])
@@ -59,10 +102,28 @@ def set(bot, update, args):
         
 
 
-def button(bot, update):
-    query = update.callback_query
-    stazione_partenza=query.data
+def printp(bot, update):
+    chat_id = update.message.chat_id
     print(stazione_partenza)
+    update.message.reply_text('La tua stazione di partenza e\': ' + stazione_partenza)
+
+def printa(bot, update):
+    chat_id = update.message.chat_id
+    print(stazione_arrivo)
+    update.message.reply_text('La tua stazione di partenza e\': ' + stazione_arrivo)
+
+def button(bot, update):
+    global stazione_partenza
+    global stazione_arrivo
+    global flag
+    query = update.callback_query
+    print(flag)
+    if flag:
+        print("p")
+        stazione_partenza=query.data
+    else:
+        print("a")
+        stazione_arrivo=query.data
     bot.edit_message_text(text="Selected option: %s" % query.data,
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
@@ -73,7 +134,7 @@ def error(bot, update, error):
 
 
 def main():
-    updater = Updater(TOKEN)
+    updater = Updater("206255782:AAE2N2fn8q_zvWhV1iaitk5okwPSxsahJyk")
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
@@ -81,8 +142,12 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("stampap", printp))
+    dp.add_handler(CommandHandler("stampaA", printa))
     dp.add_handler(CallbackQueryHandler(button))
-    dp.add_handler(CommandHandler("Partenza", set,
+    dp.add_handler(CommandHandler("setp", setp,
+                                  pass_args=True))
+    dp.add_handler(CommandHandler("seta", seta,
                                   pass_args=True))
 
     # log all errors
